332a1e00983ed1a5e447c1123e712557c37a5732
Kashifrezwi <kashifrezwi850@gmail.com>
2022-11-24 13:30:10 +0530

    updated

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Kashif Rezwi</h1>
+      <h2>Full Stack Web Developer</h2>
+    </div>
+  );
+}
+
+export default App;
