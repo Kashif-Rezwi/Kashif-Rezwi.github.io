35464a6e36f2580ee9f55ab9576876c72be5e49b
Kashif Rezwi <kashifrezwi850@gmail.com>
2026-04-04 05:36:43 +0530

    feat:  updated github stats, langs, and streaks and their style

--- src/Pages/Github.jsx
+++ src/Pages/Github.jsx
@@ -7,23 +7,31 @@ export const Github = () => {
 
             <Heading mb={"30px"} as={"h1"}>Github Calender & Stats</Heading>
 
-            <VStack w={"100%"}>
+            <VStack w={"100%"} gap={"40px"}>
                 <Box m={"auto"} p={"20px"} bg={"#f5f5f5"}>
                     <GitHubCalendar username="kashif-rezwi" />
                 </Box>
 
-                <Grid templateColumns={{ base: "repeat(1,1fr)", sm: "repeat(1,1fr)", md: "repeat(1,1fr)", lg: "repeat(2,1fr)" }} w={"100%"}>
+                {/* github info: source: http://githubreadmestatsfast.vercel.app/ */}
+                <Grid justifyItems="center" alignItems={"center"} gap={"20px"} templateColumns={{ base: "repeat(1,1fr)", sm: "repeat(1,1fr)", md: "repeat(1,1fr)", lg: "repeat(3,1fr)" }} w={"70%"}>
 
-                    <Box maxW={"380px"} m={"20px auto"} bg={"#f5f5f5"}>
+                    <Box maxW={"315px"} bg={"#f5f5f5"}>
                         <Image
-                            src="https://github-readme-stats.vercel.app/api?username=kashif-rezwi"
+                            src="https://github-readme-stats-fast.vercel.app/api?username=Kashif-Rezwi&show_icons=true"
                             alt="github stats"
                         />
                     </Box>
 
-                    <Box maxW={"380px"} m={"20px auto"} bg={"#f5f5f5"}>
+                    <Box maxW={"240px"} bg={"#f5f5f5"}>
                         <Image
-                            src="https://github-readme-streak-stats.herokuapp.com/?user=kashif-rezwi"
+                            src="https://github-readme-stats-fast.vercel.app/api/top-langs/?username=Kashif-Rezwi&layout=compact"
+                            alt="github top langs"
+                        />
+                    </Box>
+
+                    <Box maxW={"380px"} bg={"#f5f5f5"} border={"1px solid #f3ebebd0"} borderRadius={"4px"} overflow={"hidden"}>
+                        <Image
+                            src="https://github-readme-stats-fast.vercel.app/api/streak?username=Kashif-Rezwi"
                             alt="github stats"
                         />
                     </Box>
